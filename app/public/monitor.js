595618205dff4fb4d043210a51f776d42e3b0f80
dlewis <deepak.lewis@gmail.com>
2012-07-14 08:45:30 +0100

    Refactored polling code

--- app/public/monitor.js
+++ app/public/monitor.js
@@ -6,28 +6,23 @@
     self.type = ko.observable("foo");
     self.pipelines = ko.observableArray();
 
-    self.start_polling = function() {
-      setInterval(function() {
-        Radiator.MonitorStore.findById(self.options.id, function(data) {
-          screen.clearError();
-          _(data).each(function(pipeline) {
-            _(self.pipelines()).each(function(existing_pipeline) {
-              if(existing_pipeline.name() == pipeline.name) {
-                existing_pipeline.refresh(pipeline);
-              } 
-            });
-          });
-        }, screen.showError);
-      }, parseInt(self.options.refresh_rate) * 1000)
-    };
-
-    self.start = function() {
+    self.poll = function() {
       Radiator.MonitorStore.findById(self.options.id, function(data) {
+        screen.clearError();
         _(data).each(function(pipeline) {
-          self.pipelines.push(new Radiator.Pipeline(pipeline));
+          var existing_pipeline = _(self.pipelines()).find(function(p) { return p.name() == pipeline.name; });
+          if(existing_pipeline) {
+            existing_pipeline.refresh(pipeline);
+          } else {
+            self.pipelines.push(new Radiator.Pipeline(pipeline));
+          }
         });
       }, screen.showError);
-      self.start_polling();
+    }
+
+    self.start = function() {
+      self.poll();
+      setInterval(self.poll, parseInt(self.options.refresh_rate) * 1000);
     };
 
     return {
